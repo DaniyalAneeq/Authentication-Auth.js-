697235d6ed2d8c2a27cd4831897b588a1f337bed
DANIAL ANEEQ AHMED <167881750+DaniyalAneeq@users.noreply.github.com>
2025-04-10 15:33:27 +0500

    Initial commit

---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import * as z from 'zod';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { LoginSchema } from "@/schemas/index";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+
+} from "@/components/ui/form";
+
+import React from 'react';
+import { CardWrapper } from '@/components/auth/card-wrapper';
+
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  
+  return (
+    <CardWrapper
+    headerLabel='Welcome back'
+    backButtonLabel="Don't have an account?"
+    backButtonhref="/auth/register"
+    showSocial
+    >
+       <Form {...form}>
+        <form 
+        onSubmit={form.handleSubmit(() => {})}
+        className="space-y-6"
+        >
+          <div className='space-y-4'>
+            <FormField 
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input 
+                    {...field}
+                    placeholder='Enter your email'
+                    type='email'
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          </div>
+
+        </form>
+       </Form>
+    </CardWrapper>
+  )
+}
+
+export default LoginForm
